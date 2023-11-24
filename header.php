20fda0dbfa572601e4adf4f86cb9b2a5d1982587
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-11-24 23:57:58 +0800

    Add Custom Logo

--- header.php
+++ header.php
@@ -13,8 +13,9 @@
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -23,41 +24,48 @@
 
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>
 
-<div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'your-theme-name' ); ?></a>
 
-    <header id="masthead" class="site-header">
-        <div class="site-branding">
-            
-            <?php
-            if ( function_exists( 'the_custom_logo' ) ) {
-                the_custom_logo();
-            }
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text" href="#primary">
+            <?php esc_html_e('Skip to content', 'your-theme-name'); ?>
+        </a>
 
-            
-            if ( is_front_page() && is_home() ) {
-                echo '<h1 class="site-title"><a href="' . esc_url( home_url( '/' ) ) . '" rel="home">' . bloginfo( 'name' ) . '</a></h1>';
-            } else {
-                echo '<p class="site-title"><a href="' . esc_url( home_url( '/' ) ) . '" rel="home">' . bloginfo( 'name' ) . '</a></p>';
-            }
-            $description = get_bloginfo( 'description', 'display' );
-            if ( $description || is_customize_preview() ) {
-                echo '<p class="site-description">' . $description . '</p>'; 
-            }
-            ?>
-        </div><!-- .site-branding -->
 
-        <nav id="site-navigation" class="main-navigation">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'your-theme-name' ); ?></button>
-            <?php
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'menu-1',
-                    'menu_id'        => 'primary-menu',
-                )
-            );
-            ?>
-        </nav><!-- #site-navigation -->
-    </header><!-- #masthead -->
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">
+                    <?php
+                    $custom_logo_id = get_theme_mod('custom_logo');
+                    $logo = wp_get_attachment_image($custom_logo_id, 'full', false, array('loading'=> 'eager', 'decoding'=> 'async'));
+
+                    if (has_custom_logo()) {
+                        echo $logo;
+                    } else {
+                        echo '<h1>' . get_bloginfo('name') . '</h1>';
+                    }
+                    ?>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu"
+                    aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="main-menu">
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'main-menu',
+                            'container' => false,
+                            'menu_class' => '',
+                            'fallback_cb' => '__return_false',
+                            'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                            'depth' => 2,
+                            'walker' => new bootstrap_5_wp_nav_menu_walker()
+                        )
+                    );
+                    ?>
+                </div>
+            </div>
+        </nav>
