0e379440086b1f4d9a805737522a53c1fcf6ad79
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-11-24 20:08:35 +0800

    Update 404.php

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+<main class="my-auto p-5" id="content">
+  <div class="text-center py-5">
+    <h1 class="display-1">404</h1>
+    <h2>File not found</h2>
+  </div>
+</main>
+<?php
+get_footer();
