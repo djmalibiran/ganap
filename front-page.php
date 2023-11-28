dbd89126781a9c8b3ed0a809244b2df7514f5636
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-11-28 23:35:05 +0800

    Add main closing tag

--- front-page.php
+++ front-page.php
@@ -8,6 +8,14 @@ get_template_part( 'partials/section', 'features' );
 
 get_template_part( 'partials/content', 'page' );
 
+
+// get_template_part( 'partials/footer', 'homepage' );
+?>
+
+</main>
+
+<?php
+get_template_part( 'partials/section', 'latest-news' );
 get_footer();
 
 ?>
