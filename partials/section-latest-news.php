25bb9cce409def44e17aaa4a5e2ca6b1c91a488f
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-11-28 23:36:24 +0800

    Create section-latest-news.php

---
+++ partials/section-latest-news.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying Latest News and a button to main blog page.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Ganap
+ */
+?>
+
+<aside class="bg-light py-5">
+    <div class="container py-5">
+        <h2 class="text-center mb-5">Latest News</h2>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <?php
+            $recent_posts = wp_get_recent_posts(
+                array(
+                    'numberposts' => 3,
+                    'post_status' => 'publish',
+                )
+            );
+            // echo '<pre>';
+            // var_dump( $recent_posts );
+            // echo '</pre>';
+
+            foreach ( $recent_posts as $post_item ): ?>
+                <div class="col">
+                    <article class="card h-100">
+
+                        <!-- Post Thumbnail -->
+                        <?php echo wp_get_attachment_image( get_post_thumbnail_id( $post_item["ID"] ), 'large', false, array( 'class'=> 'img-fluid card-img-top', 'loading' => 'lazy', 'decoding' => 'async' ) ); ?>
+                        
+                        <div class="card-body">
+
+                            <!-- Post Title -->
+                            <h3 class="card-title"><?php echo esc_html( $post_item["post_title"] ); ?></h3>
+
+                            <!-- Post Excerpt -->
+                            <?php if ( ! has_excerpt( $post_item["ID"] ) ): ?>
+                                <p class="card-text text-body-secondary"><?php echo wp_trim_words( $post_item["post_content"], 30, '...' ); ?></p>
+                            <?php else: ?>
+                                <p class="card-text text-body-secondary"><?php echo get_the_excerpt( $post_item["ID"] ); ?></p>
+                            <?php endif; ?>
+
+                        </div>
+                    </article>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</aside>
