5710e4834d8b627b05c27f1b76a02cc11ba8ae8c
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-11-30 01:31:58 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,8 +11,8 @@
 
 ?>
 
-<footer>
-    <h2>footer</h2>
+<footer class="py-3 border-top border-secondary">
+    <p class="text-center text-body-secondary mb-0">&copy; <?php echo date("Y") . ' ' . get_bloginfo('name'); ?></p>
 </footer>
 <?php wp_footer(); ?>
 </body>
