2400a31f411a62eaa2818988a2c20725820c204d
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-12-01 22:47:30 +0800

    Inner Page Hero

--- page.php
+++ page.php
@@ -1,5 +1,15 @@
 <?php
 get_header();
+
+get_template_part( 'partials/inner', 'hero' );
+
 get_template_part( 'partials/content', 'page' );
 
+?>
+
+</main>
+
+<?php
 get_footer();
+
+?>
---
+++ partials/inner-hero.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The Hero section
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Ganap
+ */
+?>
+
+<!-- HERO Section -->
+<?php
+$page_title = get_the_title();
+$page_id = get_queried_object_id();
+
+if ( ! empty( $page_title ) ) : ?>
+    <!-- Hero Main Wrapper -->
+    <div id="hero" class="position-relative py-5 text-center" <?php echo ( ! has_post_thumbnail( $page_id ) ) ? null : 'style="background-image: url(' . esc_url( wp_get_attachment_image_url( get_post_thumbnail_id( $page_id ), 'full' ) ) . '); background-position: center; background-size: cover;"'; ?>>
+         <!-- Semi-transparent Black Overlay -->
+         <div class="d-flex position-absolute w-100 h-100 top-0 right-0 bottom-0 left-0 z-0 bg-black bg-opacity-50 user-select-none pe-none" role="presentation" tabindex="0"></div>
+            <!-- Hero Content Container -->
+            <div class="container py-5 my-5 position-relative z-1">
+                <!-- Page Title -->
+                <h1 class="display-1 text-white"><?php echo esc_html( $page_title ); ?></h1>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
